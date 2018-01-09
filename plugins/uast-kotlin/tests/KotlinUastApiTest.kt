2b84ded29606cfb02644668b369740c5eaffb8fe
Nicolay Mitropolsky <Nicolay.Mitropolsky@jetbrains.com>
2018-01-09 21:40:35 +0300

    Uast: KtLightAnnotation converting support (KT-21702)

--- plugins/uast-kotlin/tests/KotlinUastApiTest.kt
+++ plugins/uast-kotlin/tests/KotlinUastApiTest.kt
@@ -1,5 +1,6 @@
 package org.jetbrains.uast.test.kotlin
 
+import com.intellij.psi.PsiAnnotation
 import com.intellij.psi.PsiModifier
 import com.intellij.testFramework.UsefulTestCase
 import org.jetbrains.kotlin.psi.KtBinaryExpression
@@ -28,6 +29,10 @@ class KotlinUastApiTest : AbstractKotlinUastTest() {
             val toAttribute = annotation.findAttributeValue("to")!!
             assertEquals(toAttribute.evaluate(), 0)
             KtUsefulTestCase.assertInstanceOf(annotation.psi.toUElement(), UAnnotation::class.java)
+            KtUsefulTestCase.assertInstanceOf(
+                annotation.getAsJavaPsiElement(PsiAnnotation::class.java).toUElement(),
+                UAnnotation::class.java
+            )
             KtUsefulTestCase.assertInstanceOf(toAttribute.uastParent, UNamedExpression::class.java)
             KtUsefulTestCase.assertInstanceOf(toAttribute.psi.toUElement()?.uastParent, UNamedExpression::class.java)
         }
